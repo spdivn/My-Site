26c135dc663759cfad5cff24db59c084dd861e2e
Ivan <spdivn@gmail.com>
2018-10-07 19:06:40 +0200

    Set loader with some amination

--- js/main.js
+++ js/main.js
@@ -1,11 +1,29 @@
-$(function () {
+$(function() {
+    const html = document.querySelector('html');
+
+    setLoader();
+    var event = new Event('start');
+
     updateResize();
     const templateImg = (data) => `url("${data}")`;
     const templateP = (data) => `<p>${data}</p>`;
 
+    document.addEventListener('start', removeLoader);
+    function setLoader() {
+        html.classList.add('prevent-scroll');
+        html.classList.add('is-loading');
+
+    }
+    function removeLoader() {
+        setTimeout(() => {
+            html.classList.remove('prevent-scroll');
+            html.classList.remove('is-loading');
+        }, 1000);
+    }
+
     //Fetch images form json
     $.ajax('/app/json/image.json')
-        .done(function (items) {
+        .done(function(items) {
             for (let index = 0; index < items.images.length - 1; index++) {
                 let parallax = $('<div>');
                 parallax.addClass('parallax');
@@ -18,21 +36,27 @@ $(function () {
             const $parallaxDiv = $('.parallax');
             $parallaxDiv.each(index => {
                 $parallaxDiv.eq(index).css('background-image', templateImg(items.images[index].url));
+                $parallaxDiv.eq(index).css('opacity', 0);
+                $parallaxDiv.eq(index).animate({
+                    opacity: 1,
+                }, 5000);
             });
 
             const $parallaxP = $('.whiteSpace');
             $parallaxP.each(index => {
                 $parallaxP.eq(index).html(templateP(items.images[index].quotes));
             });
+
+            document.dispatchEvent(event);
         });
 
     //When resize window
-    $(window).resize(function () {
+    $(window).resize(function() {
         updateResize();
     });
 
     //When click on down arrow
-    $('#downArrow').on('click', function (event) {
+    $('#downArrow').on('click', function(event) {
         $('html, body').animate({
             scrollTop: $('.cover').outerHeight() + 4
         }, 1000);
